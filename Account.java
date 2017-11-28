cb209ef4bf93275f7f5a8ee90cde66b351b85e44
mkorosteleva <33861994+mkorosteleva@users.noreply.github.com>
2017-11-28 11:02:20 +0200

    Add files via upload

--- Account.java
+++ Account.java
@@ -1,6 +1,5 @@
-// Fig. 3.8: Account.java
-// Account class with a double instance variable balance and a constructor
-// and deposit method that perform validation.
+package chapter3;
+
 
 public class Account {
 	private String name; // instance variable
@@ -47,16 +46,3 @@ public class Account {
 		return name;
 	}
 } // end class Account
-
-/**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and * Pearson Education,
- * Inc. All Rights Reserved. * * DISCLAIMER: The authors and publisher of this
- * book have used their * best efforts in preparing the book. These efforts
- * include the * development, research, and testing of the theories and programs
- * * to determine their effectiveness. The authors and publisher make * no
- * warranty of any kind, expressed or implied, with regard to these * programs
- * or to the documentation contained in these books. The authors * and publisher
- * shall not be liable in any event for incidental or * consequential damages in
- * connection with, or arising out of, the * furnishing, performance, or use of
- * these programs. *
- *************************************************************************/
